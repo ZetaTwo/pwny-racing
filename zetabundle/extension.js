3d53f2db819da9c7f789542da8f118a89f062721
Calle Svensson <calle.svensson@zeta-two.com>
2019-02-09 01:30:40 +0100

    Added countdown graphics

--- zetabundle/extension.js
+++ zetabundle/extension.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function (nodecg) {
-	nodecg.log.info('LOG TEST');
-};
+	const countdownReplicant = nodecg.Replicant('countdown', {defaultValue: {seconds: 60*60*10, active: false}});
+
+	function updateCountdown() {
+		if(countdownReplicant.value.active) {
+			countdownReplicant.value.seconds -= 1;
+		}
+		if(countdownReplicant.value.seconds <= 0) {
+			countdownReplicant.value.seconds = 0;
+			countdownReplicant.value.active = false;
+		}
+		setTimeout(updateCountdown, 1000);
+	}
+	updateCountdown();
+}
+
