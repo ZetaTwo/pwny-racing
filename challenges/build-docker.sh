25894dc38f172e2cd1e94b48776e76bcb79a6b99
Calle Svensson <calle.svensson@zeta-two.com>
2019-03-09 02:00:40 +0100

    Added flag submitter

--- challenges/build-docker.sh
+++ challenges/build-docker.sh
@@ -7,9 +7,11 @@ DST_DIR=${CHALLENGE_NAME}_docker
 
 mkdir $DST_DIR
 cp $CHALLENGE_NAME/$CHALLENGE_NAME $DST_DIR/chall
-cp $CHALLENGE_NAME/flag $DST_DIR
+cp $CHALLENGE_NAME/.config.json $DST_DIR/.config.json
 cp docker-pwn/* $DST_DIR
+#cp $CHALLENGE_NAME/flag $DST_DIR
+cp ../submission/client/client $DST_DIR/flag_submitter
 cd $DST_DIR
-sudo docker build -t $CHALLENGE_NAME .
+docker build -t $CHALLENGE_NAME .
 echo $DST_DIR
 rm -rf $DST_DIR
