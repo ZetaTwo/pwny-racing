44ad9374af148fe0e44326d8751afdc563d1acc1
Calle Svensson <calle.svensson@zeta-two.com>
2019-03-09 01:34:14 +0100

    More dockerization

--- server/run.sh
+++ server/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -ti -p 9090:9090 --network pwnyracing --name pwnyracing-nodecg pwnyracing-nodecg
-docker run -ti -p 80:80 -p 443:443 --network pwnyracing --name pwnyracing-nginx pwnyracing-nginx
+docker run --rm -ti -p 9090:9090 --network pwnyracing --name pwnyracing-nodecg pwnyracing/nodecg
+docker run --rm -ti -p 5080:80 -p 5443:443 --network pwnyracing --name pwnyracing-nginx pwnyracing/nginx
