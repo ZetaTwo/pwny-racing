1deedd6dcdcdcf8e3a31586922bcecb1567816ac
Calle Svensson <calle.svensson@zeta-two.com>
2019-05-25 15:40:08 +0200

    Improve packaging

--- challenges/run-chall.sh
+++ challenges/run-chall.sh
@@ -3,4 +3,4 @@
 CHALLENGE_NAME=$1
 PORT=$2
 
-sudo docker run -v '/etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt' -d -p ${PORT}:1337/tcp ${CHALLENGE_NAME}:latest
+docker run -v '/etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt' -d -p ${PORT}:1337/tcp ${CHALLENGE_NAME}:latest
