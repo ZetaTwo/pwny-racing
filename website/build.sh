c737c83bb0db39354e9ca3d977c147b31cf10f81
Calle Svensson <calle.svensson@zeta-two.com>
2019-05-26 16:21:11 +0200

    Removed hardcoded aws profile name

--- website/build.sh
+++ website/build.sh
@@ -4,9 +4,9 @@ echo "Building website"
 hugo
 echo "Uploading files"
 cd public
-aws --profile pwnyracing s3 cp --recursive . s3://www.pwny.racing
+aws s3 cp --recursive . s3://www.pwny.racing
 echo "Waiting 5 seconds before clearing cache."
 sleep 5
-aws --profile pwnyracing cloudfront create-invalidation --distribution-id E7FEQCFSMPVTB --path '/*'
+aws cloudfront create-invalidation --distribution-id E7FEQCFSMPVTB --path '/*'
 cd ..
 echo "Site published"
