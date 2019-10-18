07415d006510a70fab84a2154faf9215f08f898c
Calle Svensson <calle.svensson@zeta-two.com>
2019-10-19 01:29:37 +0200

    Add pull script

---
+++ challenges/pull-all.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+./pull-chall.sh pwnable
+#./pull-chall.sh chall_status
+./pull-chall.sh chall1
+./pull-chall.sh chall2
+./pull-chall.sh chall3
+
+./pull-chall.sh chall5
+./pull-chall.sh chall6
+./pull-chall.sh chall7
+./pull-chall.sh chall8
+./pull-chall.sh chall9
+./pull-chall.sh chall10
+./pull-chall.sh chall11
+./pull-chall.sh chall12
+./pull-chall.sh chall13
+./pull-chall.sh chall14
+./pull-chall.sh chall15
+./pull-chall.sh chall16
+./pull-chall.sh chall17
+./pull-chall.sh chall18
+./pull-chall.sh chall19
